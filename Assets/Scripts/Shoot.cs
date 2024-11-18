c9669e235f1c73496cbee388aedf9de0d5c7e732
StratiX0 <jerome.hussenet@sfr.fr>
2024-11-19 00:15:44 +0100

    Added basic weapon fire
    feat :
    + Added basic shoot mechanic using raycast method

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Shoot : MonoBehaviour
+{
+    private static Shoot Instance { get; set; }
+    [Header("Input Actions")]
+    [SerializeField] private InputActionReference fire1;
+    
+    public float damage = 10f;
+
+    [SerializeField] private Camera playerCamera;
+    
+    private void Awake()
+    {
+        Instance = this;
+        fire1.action.Enable();
+    }
+
+    private void OnEnable()
+    {
+        fire1.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        fire1.action.Disable();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (fire1.action.triggered)
+        {
+            ShootFireOne();
+        }
+    }
+    
+    private void ShootFireOne()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
